994d5b7b3bc200ef665b7cb66b4e2135f6ec3138
Adriano Misina <adriano.misina@gmail.com>
2023-08-20 16:02:56 -0300

    salvando usuários em memória (Headers)

--- src/server.js
+++ src/server.js
@@ -1,7 +1,18 @@
 import http from 'node:http';
 
 const server = http.createServer((req, res) => {
-    return res.end('Hello Adriano')
+    const { method, url } = req
+    
+    if (method === 'GET' && url === '/users') {
+        return res.end('Listagem de usuários')
+    }
+
+    if (method === 'POST' && url === '/users') {
+        return res.end('Criação de usuário')
+    }
+
+    return res.end('Hello World')
+
 })
 
 server.listen(3333)
