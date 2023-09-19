5d161fe0ec2269ea37c4f4b437b90a1fc8b29136
Adriano Misina <adriano.misina@agroboard.com.br>
2023-09-19 09:16:57 -0300

    route e query parameters

--- src/routes.js
+++ src/routes.js
@@ -27,5 +27,13 @@ export const routes = [
             database.insert('users', user)
             return res.writeHead(201).end()
         }
+    },
+
+    {
+        method: 'DELETE',
+        path: '/users/',
+        handler:(req, res) => {
+            return res.end()
+        }
     }
 ]
