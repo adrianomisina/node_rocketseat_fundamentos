1250b72dc30afb79fb2c549129825098753c87c7
Adriano Misina <adriano.misina@agroboard.com.br>
2023-09-22 20:14:24 -0300

    Criando regex dos parâmetros

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
-import { Database } from './database.js';
 import { randomUUID } from 'node:crypto';
+import { Database } from './database.js';
+import { buildRoutePath } from './utils/build-route-path.js';
 
 const database = new Database
 
 export const routes = [
     {
         method: 'GET',
-        path: '/users',
+        path: buildRoutePath('/users'),
         handler:(req, res) => {
             const users = database.selec('users')
             return res.end(JSON.stringify(users))
@@ -15,7 +16,7 @@ export const routes = [
 
     {
         method: 'POST',
-        path: '/users',
+        path: buildRoutePath('/users'),
         handler:(req, res) => {
             const {name, email} = req.body
             const user = {
@@ -31,7 +32,7 @@ export const routes = [
 
     {
         method: 'DELETE',
-        path: '/users/',
+        path: buildRoutePath('/users/:id'),
         handler:(req, res) => {
             return res.end()
         }
---
+++ src/utils/build-route-path.js
@@ -0,0 +1,7 @@
+//users:/id
+
+export const buildRoutePath = (path) => {
+    const routeParametersRegex = /:([a-zA-Z]+)/g
+
+    console.log(Array.from(path.matchAll(routeParametersRegex)))
+}
